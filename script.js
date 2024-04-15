0de095adfccdd679e03f5c4454092600fd1971e8
Dino Barton <DinoBarton@protonmail.com>
2024-04-15 13:39:48 +0100

    Added Assets and added saving features.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Get all the checkboxes
+/// Get all the checkboxes
 let checkboxes = document.querySelectorAll('#myChecklist input[type="checkbox"]');
+
+// Load saved state from localStorage
+checkboxes.forEach(checkbox => {
+  let savedState = localStorage.getItem(checkbox.id);
+  checkbox.checked = savedState === 'true' ? true : false;
+});
 
 // Add event listener to each checkbox
 checkboxes.forEach(checkbox => {
@@ -7,9 +13,13 @@ checkboxes.forEach(checkbox => {
     if (this.checked) {
       // Checkbox is checked..
       console.log(this.id + ' is checked');
+      // Save state to localStorage
+      localStorage.setItem(this.id, 'true');
     } else {
       // Checkbox is not checked..
       console.log(this.id + ' is unchecked');
+      // Save state to localStorage
+      localStorage.setItem(this.id, 'false');
     }
   });
 });
